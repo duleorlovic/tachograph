cec3ac945f189431d05c86ce7c58397cdbc76218
Dusan Orlovic <orlovics@eunet.rs>
2013-05-20 08:28:25 +0200

    Mile version 1.0

--- max7219.h
+++ max7219.h
@@ -1,8 +1,4 @@
-
-#define chipSelectPin 10
 #define SPI1_write(X) SPI.transfer(X)
-
-
 
 void max7219_init1() { 
   digitalWrite(chipSelectPin, LOW);
@@ -32,22 +28,31 @@ void max7219_init1() {
 } 
  
 void display_number(unsigned int data1) { 
- 
+
   digitalWrite(chipSelectPin, LOW);
   SPI1_write(4);                 // send Thousands digit 
-  SPI1_write(data1/1000); 
+  if (data1>999)
+    SPI1_write(data1/1000); 
+  else
+    SPI1_write(0x0F);
   digitalWrite(chipSelectPin, HIGH);
- 
+  
   digitalWrite(chipSelectPin, LOW);
   SPI1_write(3);                 // send Hundreds digit 
-  SPI1_write((data1/100)%10); 
+  if (data1>99)
+    SPI1_write((data1/100)%10); 
+  else
+    SPI1_write(0x0F);
   digitalWrite(chipSelectPin, HIGH);
- 
+  
   digitalWrite(chipSelectPin, LOW);
   SPI1_write(2);                 // send Tens digit 
-  SPI1_write((data1/10)%10); 
+  if (data1>9)
+    SPI1_write((data1/10)%10); 
+  else
+    SPI1_write(0x0F);
   digitalWrite(chipSelectPin, HIGH);
- 
+  
   digitalWrite(chipSelectPin, LOW);
   SPI1_write(1);                 // send Ones digit 
   SPI1_write(data1%10); 
