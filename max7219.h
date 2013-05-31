9728884d1234d721be358b4538e7001c16016b7f
dusan <dusan@orlovic-dell.(none)>
2013-05-31 22:50:38 +0200

    some max7219 spi bugs

--- max7219.h
+++ max7219.h
@@ -3,7 +3,7 @@
 void max7219_init1() { 
   digitalWrite(chipSelectPin, LOW);
   SPI1_write(0x09);      // BCD mode for digit decoding 
-  SPI1_write(0xFF); 
+  SPI1_write(0x0F); 
   digitalWrite(chipSelectPin, HIGH);
  
   digitalWrite(chipSelectPin, LOW);
@@ -13,18 +13,19 @@ void max7219_init1() {
  
   digitalWrite(chipSelectPin, LOW);
   SPI1_write(0x0B); 
-  SPI1_write(0x07);      // Display refresh 
+  SPI1_write(0x03);      // Display refresh 0x07
+  digitalWrite(chipSelectPin, HIGH);
+ 
+  digitalWrite(chipSelectPin, LOW);
+  SPI1_write(0x0F); 
+  SPI1_write(0x00);      // No test 
   digitalWrite(chipSelectPin, HIGH);
  
   digitalWrite(chipSelectPin, LOW);
   SPI1_write(0x0C); 
   SPI1_write(0x01);      // Turn on the display 
   digitalWrite(chipSelectPin, HIGH);
- 
-  digitalWrite(chipSelectPin, LOW);
-  SPI1_write(0x00); 
-  SPI1_write(0xFF);      // No test 
-  digitalWrite(chipSelectPin, HIGH);
+
 } 
  
 void display_number(unsigned int data1) { 
@@ -58,3 +59,51 @@ void display_number(unsigned int data1) {
   SPI1_write(data1%10); 
   digitalWrite(chipSelectPin, HIGH);
 } 
+
+ 
+void display_increment_number(unsigned int data1) { 
+
+  if (data1%10 != 0)
+  {
+    digitalWrite(chipSelectPin, LOW);
+    SPI1_write(1);                 // send Ones digit 
+    SPI1_write(data1%10); 
+    digitalWrite(chipSelectPin, HIGH);
+    return;
+  }
+  digitalWrite(chipSelectPin, LOW);
+  SPI1_write(1);                 // send Ones digit as 0
+  SPI1_write(0); 
+  digitalWrite(chipSelectPin, HIGH);
+    
+  if ((data1/10)%10 != 0)
+  {
+    digitalWrite(chipSelectPin, LOW);
+    SPI1_write(2);                 // send Tens digit 
+    SPI1_write((data1/10)%10);    
+    digitalWrite(chipSelectPin, HIGH);
+    return;   
+  }
+  digitalWrite(chipSelectPin, LOW);
+  SPI1_write(2);                 // send Tens digit 
+  SPI1_write(0);    
+  digitalWrite(chipSelectPin, HIGH);
+  
+  if ((data1/100)%10 != 0)
+  {
+    digitalWrite(chipSelectPin, LOW);
+    SPI1_write(3);                 // send Hundreds digit 
+    SPI1_write((data1/100)%10);
+    digitalWrite(chipSelectPin, HIGH);
+    return;
+  }  
+  digitalWrite(chipSelectPin, LOW);
+  SPI1_write(3);                 // send Hundreds digit 
+  SPI1_write(0);
+  digitalWrite(chipSelectPin, HIGH);
+      
+  digitalWrite(chipSelectPin, LOW);
+  SPI1_write(4);                 // send Thousands digit 
+  SPI1_write(data1/1000); 
+  digitalWrite(chipSelectPin, HIGH);
+}
